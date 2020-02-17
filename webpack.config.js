351b36d78a63cd5d89dd2ff58a7e575863d2fedf
Oliver Brandt <oliver.brandt@hamburg.de>
2020-02-17 20:24:00 +0100

    Move webpack config to top level

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,5 @@ const path = require('path'); // eslint-disable-line no-unused-vars
 const webpack = require('webpack'); // eslint-disable-line no-unused-vars
 
 module.exports = {
-
+  entry: './src/client/index.js',
 };
