957c8cc80d62ea120d8f3a95b6b2b575cca2edcc
oliver-hh <oliver.brandt@hamburg.de>
2020-02-19 17:51:53 +0100

    Add file-loader and ignore dist-folder

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,10 @@
 import {checkForName} from './js/nameChecker';
 import {handleSubmit} from './js/formHandler';
+import {img} from './images/udacity-wfed.jpg';
 
 console.log('--> index.js logging');
 console.log(checkForName);
 console.log(handleSubmit);
+console.log(img);
 
 alert('I EXIST');
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
         exclude: '/node_modules',
         loader: 'babel-loader',
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+      },
     ],
   },
 };
